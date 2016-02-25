fe4f5ce56e479fc5b0b5350315e2c4c850876837
Waterstrong <sqlin@thoughtworks.com>
2016-02-25 22:02:18 +0800

    [#CONWAY Waterstrong] - refactor generateNext rules

--- src/main/java/kata/conway/Cell.java
+++ src/main/java/kata/conway/Cell.java
@@ -3,9 +3,7 @@ package kata.conway;
 import static kata.conway.CellStatus.*;
 
 public class Cell {
-    private static final int TWO = 2;
-    private static final int THREE = 3;
-    private final CellStatus status;
+    private CellStatus status;
     private int aliveNeighbourCount;
 
     public Cell(CellStatus status) {
@@ -13,13 +11,8 @@ public class Cell {
     }
 
     public Cell generateNext() {
-        if(status == DEAD && aliveNeighbourCount == THREE) {
-            return new Cell(ALIVE);
-        }
-        if(status == ALIVE && aliveNeighbourCount >= TWO && aliveNeighbourCount <= THREE) {
-            return new Cell(ALIVE);
-        }
-        return new Cell(DEAD);
+        return ((status == ALIVE && aliveNeighbourCount == 2) || aliveNeighbourCount == 3) ?
+                new Cell(ALIVE) : new Cell(DEAD);
     }
 
     public void setAliveNeighbourCount(int aliveNeighbourCount) {
